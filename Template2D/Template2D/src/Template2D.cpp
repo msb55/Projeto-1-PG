5adc44a930a6f06258a18972e03a8501b17f14ea
Marcos <msb5@cin.ufpe.br>
2016-11-13 19:38:45 -0300

    Atualização do projeto

--- Template2D/Template2D/src/Template2D.cpp
+++ Template2D/Template2D/src/Template2D.cpp
@@ -2,27 +2,47 @@
 using std::cerr;
 using std::endl;
 #include <stdlib.h>
-//using std::exit;
-#include <GL/glut.h> // GLUT stuff, includes OpenGL headers as well 
+#include <GL/glut.h> 
 #include <windows.h>
-#include <math.h>
+#include <cmath>
 #include <gl/Gl.h>
 #include <gl/Glu.h>
+#include <vector>
+
+//Leitura de arquivo
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 
 int SCREEN_HEIGHT = 480;
-// Keep track of times clicked, on 3 clicks draw.
 int NUMPOINTS = 0;
+int numvetor = 0;
+bool showControllerPoints = true;
+bool showControlPoligonal = true;
+bool showCurveDots = true;
+bool bezierCurveControl = true;
 
-// Point class to keep it a little cleaner.
+int contadora = 0;
+int cont = 0;
+float largura = 7;//DEFAULT
+float avaliacoes = 1000;//DEFAULT
+bool plota = false;
+
+
+
 class Point {
 public:
-	float x, y, z;
-	void setxy(float x2, float y2) { x = x2; y = y2; }
+	double x, y, z;
+	void setxy(double x2, double y2) { x = x2; y = y2; }
 
 };
 
-Point points[4];
+Point lastPoint;
+vector<Point> points;
+vector<Point> deltaOnePontos;
+vector<Point> arc;
 
 void myInit() {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -31,7 +51,20 @@ void myInit() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+	lastPoint.setxy(0.0, 0.0);
 
+	string slargura, savaliacoes;
+	ifstream myfile("in.txt");//arquivo deve estar em \bin do projeto :)
+	if (myfile.is_open())
+	{
+		getline(myfile, slargura);
+		getline(myfile, savaliacoes);
+
+		largura = atof(slargura.c_str());
+		avaliacoes = atof(savaliacoes.c_str());
+
+		myfile.close();
+	}
 }
 
 void drawDot(int x, int y) {
@@ -51,56 +84,177 @@ void drawLine(Point p1, Point p2) {
 }
 
 
-Point Casteljau(int i, int j, float t) {
-	if (i == 0) {
-		return points[j];
-	}
-	Point P;
+Point Casteljau(vector<Point> pontos1, int n, double t) {//CASTELJÃO DA MASSA 2.1
 
-	P.x = (1 - t)*Casteljau(i - 1, j, t).x + t*Casteljau(i - 1, j + 1, t).x;
-	P.y = (1 - t)*Casteljau(i - 1, j, t).y + t*Casteljau(i - 1, j + 1, t).y;
+	vector<Point> pontos = pontos1;
+	int qtd = n;
+	int limite = n;
+	int inicio = 0;
+	Point P; int i;
 
-	return P;
+	do {
+		for (i = inicio; i < limite - 1; i++) {
+			P.x = (1 - t)*pontos[i].x + t*pontos[i + 1].x;
+			P.y = (1 - t)*pontos[i].y + t*pontos[i + 1].y;
+			pontos.push_back(P);
+		}
+		qtd--;
+		limite = limite + qtd;
+		inicio = i + 1;
+
+	} while (qtd != 1);
+
+	return pontos[inicio];
 }
 
-void myMouse(int button, int state, int x, int y) {
-	// If left button was clicked
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		// Store where the user clicked, note Y is backwards.
-		points[NUMPOINTS].setxy((float)x, (float)(SCREEN_HEIGHT - y));
-		NUMPOINTS++;
+void DrawCircle(float cx, float cy, float x0, float y0, float r, int num_segments)
+{
+	float theta = 1 * 3.1415926 / float(num_segments);
+	float c = cosf(theta);
+	float s = sinf(theta);
+	float t;
+	float x = x0;
+	float y = y0;
 
-		// Draw the red  dot.
-		drawDot(x, SCREEN_HEIGHT - y);
+	Point P0;
+	Point P1;
+	P0.setxy(x + cx, y + cy);
+	glColor3f(0.0, 0.0, 1.0);
 
-		// If 3 points are drawn do the curve.
-		if (NUMPOINTS > 3) {
-			glClear(GL_COLOR_BUFFER_BIT);
-			glFlush();
+	for (int ii = 1; ii < num_segments; ii++) {
+		t = x;
+		x = c * x - s * y;
+		y = s * t + c * y;
+		P1.setxy(x + cx, y + cy);
+		drawLine(P0, P1);
 
+
+		P0 = P1;
+	}
+
+	glFlush();
+}
+
+Point metodo(Point vetorTangente, Point pontoAcostamento) {
+
+	if (!((abs(vetorTangente.y) <= 0.163 && abs(vetorTangente.x) <= 0.163))) {
+
+		plota = false;
+		Point Pn, Pc;
+
+		Pn.setxy(pontoAcostamento.x - lastPoint.x, pontoAcostamento.y - lastPoint.y);
+		if (lastPoint.x == 0.0 && lastPoint.y == 0.0) return vetorTangente;
+
+
+		Pn.x = Pn.x / (sqrt(pow(Pn.x, 2) + pow(Pn.y, 2)));
+		Pn.y = Pn.y / (sqrt(pow(Pn.x, 2) + pow(Pn.y, 2)));
+
+
+		Pc.x = 0.5*lastPoint.x + 0.5*pontoAcostamento.x;
+		Pc.y = 0.5*lastPoint.y + 0.5*pontoAcostamento.y;
+
+
+		if ((abs(lastPoint.x - pontoAcostamento.x) > 0.1) && (abs(lastPoint.y - pontoAcostamento.y) > 0.1)) {
+			DrawCircle(Pc.x, Pc.y, (pontoAcostamento.x - Pc.x), (pontoAcostamento.y - Pc.y), largura / 2, 50);
+		}
+	}
+
+	return vetorTangente;
+}
+
+Point acostamento(Point pontoReferencia, Point pontoReferencia2, float largura) {
+	Point vetorTangente, vetorNormal, pontoAcostamento;
+	float aux, x, y;
+	// VETOR TANGENTE
+	vetorTangente.x = pontoReferencia.x - pontoReferencia2.x;
+	vetorTangente.y = pontoReferencia.y - pontoReferencia2.y;
+
+	x = vetorTangente.x;
+	y = vetorTangente.y;
+
+	Point vT = vetorTangente;
+	//NORMALIZA VETOR TANGENTE
+	vetorTangente.x = vetorTangente.x / (sqrt(pow(vetorTangente.x, 2) + pow(vetorTangente.y, 2)));
+	vetorTangente.y = vetorTangente.y / (sqrt(pow(vetorTangente.x, 2) + pow(vetorTangente.y, 2)));
+	//CALCULA NORMAL
+	vetorNormal.x = (-1) * vetorTangente.y;
+	vetorNormal.y = vetorTangente.x;
+	//PONTO DO ACOSTAMENTO
+	pontoAcostamento.x = pontoReferencia.x + vetorNormal.x * largura;
+	pontoAcostamento.y = pontoReferencia.y + vetorNormal.y * largura;
+
+	if (((abs(y) <= 0.163 && abs(x) <= 0.163) || plota) && cont != 0) {
+		plota = true;
+		metodo(vT, pontoAcostamento);
+	}
+	else {
+		lastPoint = pontoAcostamento;
+	}
+
+	return pontoAcostamento;
+}
+
+void draw() { //DESENHA OBJETOS NA TELA
+
+	cont = 0;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+
+	if (showControllerPoints) {
+		for (int i = 0; i < NUMPOINTS; i++) {
+			drawDot(points[i].x, points[i].y);//PONTOS DE CONTROLE
+		}
+	}
+
+
+
+	if (NUMPOINTS >= 3) { //DESENHA CURVA A PARTIR DE 3 PONTOS
+
+		if (showControlPoligonal) {
 			glColor3f(1.0, 1.0, 1.0);
 			for (int i = 0; i < NUMPOINTS - 1; i++) {
-				drawLine(points[i], points[i + 1]);
+				drawLine(points[i], points[i + 1]); //POLIGONAL DE CONTROLE
 			}
-
-			for (int i = 0; i < NUMPOINTS; i++) {
-				printf("pont j000 = %d valor x = %f valor y = %f \n", i, points[i].x, points[i].y);
-			}
-			// Draw two legs of the triangle
+		}
 
 
-			//drawLine(abc[3], abc[4]);
-			Point POld = points[0];
-			/* Draw each segment of the curve.  Make t increment in
-			smaller amounts for a more detailed curve. */
-			for (double t = 0.0; t <= 1.0; t += 0.01) {
-				Point P = Casteljau(NUMPOINTS - 1, 0, t);
-				drawLine(POld, P);
-				POld = P;
+		glColor3f(0.5, 0.5, 1.0);
+		glPointSize(2.0);
+
+
+		Point POld = points[0];
+		Point PCOld = points[0];
+		float x = 1 / avaliacoes;
+		for (double t = 0.0; t <= 1.0; t += x) {
+			Point P1 = Casteljau(points, NUMPOINTS, t);//CALCULA PONTO NA CURVA DE BEZIER
+			Point P2 = acostamento(P1, POld, largura); //CALCULA PONTO DO ACOSTAMENTO
+			if (bezierCurveControl) {//DESENHA CURVA DE BEZIER
+				glColor3f(1.0, 0.0, 0.0);
+				drawLine(POld, P1);
 			}
-			glColor3f(1.0, 0.0, 0.0);
+			cont++;
+			if (!plota) {
+
+				glColor3f(0.0, 0.0, 1.0); //DESENHA ACOSTAMENTO
+				if (bezierCurveControl) drawLine(PCOld, P2);
+			}
+			PCOld = P2;
+			POld = P1;
 
 		}
+		glColor3f(1.0, 0.0, 0.0);
+	}
+}
+
+
+void myMouse(int button, int state, int x, int y) {//GUARDA COORDENADAS DO MOUSE	
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		Point p;
+		p.setxy((double)x, (double)(SCREEN_HEIGHT - y));
+		points.push_back(p);
+		NUMPOINTS++;
+		draw();
 	}
 }
 
@@ -109,15 +263,37 @@ void myDisplay() {
 	glFlush();
 }
 
+void hadleSpecialKeyboard(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_F1: //SHOW POLIGONAL DE CONTROLE
+		showControlPoligonal = !showControlPoligonal;
+		draw();
+		break;
+	case GLUT_KEY_F2://SHOW PONTOS DE CONTROLE
+		showControllerPoints = !showControllerPoints;
+		draw();
+		break;
+	case GLUT_KEY_F3: //SHOW PONTOS DA CURVA
+		showCurveDots = !showCurveDots;
+		draw();
+		break;
+	case GLUT_KEY_F4: //SHOW CURVA DE BEZIER
+		bezierCurveControl = !bezierCurveControl;
+		draw();
+		break;
+	}
+}
+
 int main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(100, 150);
-	glutCreateWindow("Bezier Curve");
+	glutCreateWindow("Estrada de Bezier");
 
 	glutMouseFunc(myMouse);
 	glutDisplayFunc(myDisplay);
+	glutSpecialUpFunc(hadleSpecialKeyboard);
 
 	myInit();
 	glutMainLoop();
